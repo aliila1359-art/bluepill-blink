83526d4cc7419ae66bc561fdff4554405f3ac2a9
aliila1359-art <aliila1359@gmail.com>
2026-07-28 06:02:00 +0330

    Create startup.c

---
+++ startup.c
@@ -0,0 +1,23 @@
+typedef void (*func_ptr)(void);
+
+extern void main(void);
+extern unsigned int _sidata, _data, _edata, _sbss, _ebss;
+
+void Reset_Handler(void) {
+    unsigned int *src = &_sidata;
+    unsigned int *dst = &_data;
+    while (dst < &_edata) {
+        *dst++ = *src++;
+    }
+    dst = &_sbss;
+    while (dst < &_ebss) {
+        *dst++ = 0;
+    }
+    main();
+}
+
+__attribute__((section(".isr_vector")))
+func_ptr vector_table[] = {
+    (func_ptr0x20005000), // Stack Pointer
+    (func_ptr)Reset_Handler
+};
